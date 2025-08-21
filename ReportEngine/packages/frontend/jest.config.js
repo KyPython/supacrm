cc249445bfe0188b4c59edd6bd9893726a681932
KyPython <kyjahntsmith@gmail.com>
2025-08-20 21:11:56 -0400

    Delete ReportEngine directory

--- ReportEngine/packages/frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/test/test-setup.js'],
-  moduleFileExtensions: ['js', 'jsx', 'json'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest'
-  },
-  testPathIgnorePatterns: ['/node_modules/', '/dist/']
-};
