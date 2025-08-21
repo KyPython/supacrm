8b54caf55a4f3b8fb5dd88c52b295c8f25c92783
KyPython <kyjahntsmith@gmail.com>
2025-08-21 01:07:30 -0400

    chore: finalize repository cleanup and vercel config

--- ReportEngine/ReportEngine/ReportEngine/packages/frontend/test/minimal.test.js
+++
@@ -1,4 +0,0 @@
-test('minimal sync test', () => {
-  console.log('minimal running')
-  expect(2).toBe(2)
-})
--- ReportEngine/ReportEngine/ReportEngine/packages/frontend/test/sanity.test.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-
-test('sanity test - runs', () => {
-  expect(1 + 1).toBe(2)
-})
